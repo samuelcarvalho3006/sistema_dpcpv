b1983b1adfb51a7b9bdc57dd4febd8e42600476a
Samuel de Jesus <samuel.j.a.carvalho@gmail.com>
2024-08-16 18:47:51 -0300

    atuali. 18:47 16/08

--- cadastroPedidos.php
+++ cadastroPedidos.php
@@ -73,6 +73,69 @@ try {
 
 <body>
 
+  <div class="container-fluid cabecalho"> <!-- CABECALHO -->
+    <nav class="navbar fixed-top navbar-light navbar-expand-md" style="background-color: #FFFF;">
+      <a class="navbar-brand m-2" href="#">
+        <img src="./img/logoPreta.png">
+      </a>
+
+      <button class="navbar-toggler hamburguer" data-toggle="collapse" data-target="#navegacao">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navegacao">
+
+        <ul class="nav nav-pills justify-content-end listas"> <!-- LISTAS DO MENU CABECALHO-->
+
+
+          <li class="nav-item dropdown"> <!-- LINK BOOTSTRAP DORPDOWN MENU-->
+            <a class="nav-link dropdown-toggle cor_fonte" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Pedidos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="cadastroPedidos.php">Cadastro de Pedidos</a>
+              <a class="dropdown-item" href="consultaPedidos.php">Consulta de Pedidos</a>
+            </div>
+          </li> <!-- FECHA O DROPDOWN MENU-->
+
+          <li class="nav-item dropdown"> <!-- LINK BOOTSTRAP DORPDOWN MENU-->
+            <a class="nav-link dropdown-toggle cor_fonte" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Orçamentos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="cadastroOrcamentos.php">Cadastro de Orçamentos</a>
+              <a class="dropdown-item" href="consultaOrcamentos.php">Consulta de Orçamentos</a>
+            </div>
+          </li> <!-- FECHA O DROPDOWN MENU-->
+
+          <li class="nav-item dropdown"> <!-- LINK BOOTSTRAP DORPDOWN MENU-->
+            <a class="nav-link dropdown-toggle cor_fonte" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Produtos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="cadastroProdutos.php">Cadastro de Produtos</a>
+              <a class="dropdown-item" href="edicaoProdutos.php">Edição de Produtos</a>
+            </div>
+          </li> <!-- FECHA O DROPDOWN MENU-->
+
+          <li>
+            <a href="logout.php" class="nav-link" style="color: red;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+                <path fill-rule="evenodd"
+                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+              </svg>
+            </a>
+          </li>
+        </ul> <!-- FECHA LISTAS MENU CABECALHO -->
+      </div>
+    </nav> <!-- FECHA CABECALHO -->
+  </div> <!-- FECHA CONTAINER DO CABECALHO -->
 
 
   <div class="container cad">
